5dc19ac16bae21f5df53039fcf36d841a9501b1e
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2022-09-17 13:46:48 +0700

    feat: :sparkles: add promise function for calculate films's results

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (type) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const [...films] = await Promise.all([
+        promiseTheaterIXX(),
+        promiseTheaterVGC(),
+      ]);
+      const allFilmsTheaters = films.flat();
+      const filteredFilms = allFilmsTheaters.filter((x) => x.hasil === type);
+      resolve(filteredFilms.length || 0);
+    } catch (error) {
+      reject("ada kesalahan dalam pengambilan data");
+    }
+  });
+};
 
 module.exports = {
   promiseOutput,
